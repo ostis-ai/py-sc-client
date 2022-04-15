2f317e64349d65ea475102f8f4f9d8b5da01fc65
Alexandr <alexandr.zagorskiy.work@gmail.com>
2022-04-15 14:55:58 +0300

    feat(api): provide only api methods for import

---
+++ src/sc_client/client/__init__.py
@@ -0,0 +1,7 @@
+from sc_client.client._api import (
+    connect, disconnect, is_connected,
+    create_elements, check_elements, delete_elements,
+    set_link_contents, get_link_content, get_link_by_content,
+    resolve_keynodes, template_search, template_generate,
+    events_create, events_destroy, is_event_valid
+)
