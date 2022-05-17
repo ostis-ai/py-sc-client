9b843d6ba3838764a5a33340b9f82bef07efdfac
Alexandr <alexandr.zagorskiy.work@gmail.com>
2022-05-17 21:42:02 +0300

    feat: change package installation source

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 DIRECTORY_PATH = Path(__file__).parent
 README = (DIRECTORY_PATH / "README.md").read_text()
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 INSTALL_REQUIRES = ["websocket-client>=1.0.1"]
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3, 8)
@@ -29,7 +29,7 @@ if CURRENT_PYTHON < REQUIRED_PYTHON:
 
 
 setup(
-    name="Py-sc-client",
+    name="py-sc-client",
     version=VERSION,
     description="The Python implementation of the client for communication with the sc-server",
     long_description=README,
