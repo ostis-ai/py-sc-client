97e4f356393761340a40ce017657cc8bab601b71
FalseR20 <kno3.2002@gmail.com>
2023-09-22 11:54:21 +0300

    docs: change changelog and setup file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 DIRECTORY_PATH = Path(__file__).parent
 README = (DIRECTORY_PATH / "README.md").read_text()
 
-VERSION = "0.3.0"
+VERSION = "0.4.0"
 INSTALL_REQUIRES = ["websocket-client>=1.0.1"]
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3, 8)
